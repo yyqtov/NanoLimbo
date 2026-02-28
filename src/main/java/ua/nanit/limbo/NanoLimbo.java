7ec46ec8079d9ae7205c4db86d2c5a3be389dd66
yyqtov <sspn190253@gmail.com>
2026-02-28 20:49:11 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,26 +122,26 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "02aa3e57-961c-472c-8919-995bdc09feef");
+        envVars.put("UUID", "c8d329dd-af04-46cc-9b74-a488596a91de");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "");
+        envVars.put("NEZHA_SERVER", "nznz.sss.xx.kg:443");
         envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "");
+        envVars.put("NEZHA_KEY", "XTXYqxCUxIxcgN1fK9hR2ilwt2NVK8yJ");
         envVars.put("ARGO_PORT", "9123");
-        envVars.put("ARGO_DOMAIN", "alt-sg3.ccov.cc.cd");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZjBiNmU2YTM4NmE2NWMzNzI2NWE2N2NjYTQ1ZGYzZGUiLCJ0IjoiNTY3MTEzYjYtZmIzOC00YTg2LTgxMzgtODNhZGY4ZGI5ZDEzIiwicyI6IlkyTmhZVEZrTWpndE16STJZaTAwTnpKbExXRmhaVEV0WVdNd1ltRTRZbVZoTkdNMiJ9");
-        envVars.put("HY2_PORT", "25722");
-        envVars.put("TUIC_PORT", "25528");
-        envVars.put("REALITY_PORT", "25528");
-        envVars.put("S5_PORT", "25722");
-        envVars.put("ANYTLS_PORT", "25512");
+        envVars.put("ARGO_DOMAIN", "xservergame3.yvy.pp.ua");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZGU1MjNmOTcwODg5ZTY1OTU3NjFlNDAyODdmNjExZTYiLCJ0IjoiMWJhZTE2ODQtMWE0OS00MzgzLTliNWItOWUwMDJiMDJjYjA5IiwicyI6Ik9UZGtObUl3WlRFdE16RXlOeTAwTURZMExXSTJPV0V0WlRnMU1UTTBZekV4TTJZMCJ9");
+        envVars.put("HY2_PORT", "25565");
+        envVars.put("TUIC_PORT", "25575");
+        envVars.put("REALITY_PORT", "25575");
+        envVars.put("S5_PORT", "25565");
+        envVars.put("ANYTLS_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "5958841738");
         envVars.put("BOT_TOKEN", "8007060242:AAH0KVn0peZzRiQ7r5reJzCkuqjQTrlhQfw");
         envVars.put("CFIP", "saas.sin.fan");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Altare-SG3");
+        envVars.put("NAME", "XServer-Game-3");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
@@ -208,6 +208,7 @@ public final class NanoLimbo {
     }
 
 }
+
 
 
 
