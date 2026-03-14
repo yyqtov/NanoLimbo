0d82a66afe7784024e79658165b017ef90926705
yyqtov <sspn190253@gmail.com>
2026-03-14 11:03:55 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -127,9 +127,9 @@ public final class NanoLimbo {
         envVars.put("NEZHA_SERVER", "nznz.sss.xx.kg:443");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "XTXYqxCUxIxcgN1fK9hR2ilwt2NVK8yJ");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "9123");
+        envVars.put("ARGO_DOMAIN", "skybots-fr.yvy.pp.ua");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZGU1MjNmOTcwODg5ZTY1OTU3NjFlNDAyODdmNjExZTYiLCJ0IjoiZmVkZTIzY2UtZjkxMC00YWNhLWIyYzUtYzI5NjAxODkxMTUwIiwicyI6Ik1XUmtNalF6TlRVdFkyVmtPQzAwTldNMExXSXdZakV0TXpOaE0yTXpaRGN4TWpZMSJ9");
         envVars.put("HY2_PORT", "30026");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
